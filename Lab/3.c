c938079e03ca53bb9320d152033bd3b208228930
green-mint <saad.521509@gmail.com>
2021-02-14 22:07:02 +0500

    version 0.1.0

---
+++ Lab/3.c
@@ -0,0 +1,20 @@
+// Syed Asadullah Kashif (CMS: 333516) BSCS 10B
+
+#include <stdio.h>
+
+int main()
+{
+    // A constant indicating the value of pi
+    const float pi = 3.14159;
+
+    float radius = 0;
+
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    printf("Diameter of the cicle is %f\n", 2 * radius);
+    printf("Circumference of the cicle is %f\n", 2 * pi * radius);
+    printf("Area of the cicle is %f\n", pi * radius + radius);
+
+    return 0;
+}
